b91da7e6ec1f223c78357b37b0f68d0b7381650a
Bo Gao <bgao@o-netusa.com>
2020-12-31 23:03:32 -0500

    Add network module (wip)

---
+++ network/TcpClient.h
@@ -0,0 +1,16 @@
+/**************************************************************************
+ * @file:  TcpClient.h
+ * @brief:
+ *
+ * Copyright(c) 2020-present O-Net Communications (ShenZhen) Limited.
+ * This code is licensed under MIT license (see LICENSE for details)
+ *
+ *************************************************************************/
+
+#pragma once
+
+#ifdef _WIN32
+#include <WinSock2.h>
+#endif
+
+#include <asio.hpp>
---
+++ tests/network/TcpTests.cpp
@@ -0,0 +1,21 @@
+/**************************************************************************
+ * @file:  TcpTests.cpp
+ * @brief:
+ *
+ * Copyright(c) 2020-present O-Net Communications (ShenZhen) Limited.
+ * This code is licensed under MIT license (see LICENSE for details)
+ *
+**************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include <network/TcpClient.h>
+#include <network/TcpServer.h>
+
+TEST(TCPTests, TCPServer)
+{
+}
+
+TEST(TCPTests, TCPClient)
+{
+}
