52910f0b9b7d9a86b6335cfc78acdc7c01ede393
Bo Gao <bgao@o-netusa.com>
2022-05-24 22:28:13 -0400

    Revert BlockingQueue.h

--- common/BlockingQueue.h
+++ common/BlockingQueue.h
@@ -21,50 +21,61 @@ template <typename T>
 class BlockingQueue
 {
 public:
-    BlockingQueue(uint32_t max_size = 0)
-        : m_max_size(max_size)
-    {}
+    BlockingQueue() = default;
     ~BlockingQueue() = default;
 
     void Push(const T& data)
     {
         {
             std::lock_guard<std::mutex> lock(m_mutex);
-            while (m_max_size > 0 && m_queue.size() >= m_max_size)
-            {
-                m_queue.pop();
-            }
             m_queue.push(data);
         }
         m_signal.notify_one();
     }
 
-    T Pop()
+    void Pop(T& data)
     {
         std::unique_lock<std::mutex> lock(m_mutex);
         while (m_queue.empty())
             m_signal.wait(lock);
 
-        auto data = m_queue.front();
+        data = m_queue.front();
         m_queue.pop();
-        return data;
     }
 
-    T TryPop(int32_t timeout_ms = 0)
+    void Pop(T& data, const std::function<bool()>& f)
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        while (m_queue.empty() && f())
+            m_signal.wait(lock);
+
+        if (!f())
+            return;
+        data = m_queue.front();
+        m_queue.pop();
+    }
+
+    void PopFuncNotify()
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_signal.notify_one();
+    }
+
+    bool TryPop(T& value, int32_t timeout_ms = 0)
     {
         std::unique_lock<std::mutex> lock(m_mutex);
         if (m_queue.empty())
         {
             if (m_signal.wait_for(lock, std::chrono::milliseconds(timeout_ms)) ==
                 std::cv_status::timeout)
-                throw std::runtime_error("BlockingQueue::TryPop timeout");
+                return false;
 
             if (m_queue.empty())
-                throw std::runtime_error("BlockingQueue::TryPop empty");
+                return false;
         }
-        auto value = m_queue.front();
+        value = m_queue.front();
         m_queue.pop();
-        return value;
+        return true;
     }
 
     bool Empty() const { return m_queue.empty(); }
@@ -79,7 +90,6 @@ public:
     size_t Size() const { return m_queue.size(); }
 
 private:
-    uint32_t m_max_size{0};
     std::queue<T> m_queue;
     mutable std::mutex m_mutex;
     std::condition_variable m_signal;
